4ad2f6d7340ee721e12a86221ff4b01f76cbd02e
sone <sthaya@gmail.com>
2022-01-07 10:51:06 -0800

    added list recipe feature

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -1,7 +1,6 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 from django.urls import reverse
-from django.urls.conf import path
 
 from rest_framework.test import APIClient
 from rest_framework import status
